d93483b74165fb01d51b2c4cc570a1dd4f6c8f88
ihsanvp <ihsanvp2738@gmail.com>
2023-02-12 18:54:57 +0530

    feature(desktop): show app after render

--- apps/desktop/src/routes/+layout.svelte
+++ apps/desktop/src/routes/+layout.svelte
@@ -2,10 +2,16 @@
   import { afterNavigate } from "$app/navigation";
   import Titlebar from "$lib/components/titlebar/Titlebar.svelte";
   import { previousPage } from "$lib/utils/navigation";
+  import { onMount } from "svelte";
+  import { invoke } from "@tauri-apps/api";
   import "../app.css";
 
   afterNavigate(({ from }) => {
     previousPage.set(from?.url?.pathname || $previousPage);
+  });
+
+  onMount(() => {
+    invoke("show_main_window");
   });
 </script>
 
